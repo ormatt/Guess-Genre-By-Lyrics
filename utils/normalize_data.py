20fadb19066b13c37ffd84adc1e130d9ff57e156
ormatt <ormatt@outlook.com>
2017-07-31 19:59:14 +0300

    Optimize get_parent_genre method so that string matching is not performed unless found genre is not listed in the genres dictionary

--- utils/normalize_data.py
+++ utils/normalize_data.py
@@ -4,23 +4,30 @@ from fuzzywuzzy import process
 
 
 def get_parent_genre(found_genre):
-    tokens_list = constants.WORD_TOKENIZER.tokenize(found_genre)
-    found_genre = ' '.join(tokens_list)
+    found_genre = found_genre.lower()
+    if found_genre in constants.GENRES_TREE:
+        decided_genre = constants.GENRES_TREE[found_genre]
+        closest_genre = decided_genre
+        score = 100
 
-    # Get the closest matching listed genre, and a similarity score
-    if not found_genre:
-        decided_genre = constants.DEFAULT_GENRE
-        closest_genre = ''
-        score = 0
     else:
-        closest_genre, score = process.extractOne(found_genre,
-                                                  constants.ALL_GENRES_LIST)
+        tokens_list = constants.WORD_TOKENIZER.tokenize(found_genre)
+        found_genre = ' '.join(tokens_list)
 
-        if score > constants.GENRE_SIMILARITY_THRESHOLD:
-            parent_genre = constants.GENRES_TREE[closest_genre]
-            decided_genre = parent_genre
-        else:
+        # Get the closest matching listed genre, and a similarity score
+        if not found_genre:
             decided_genre = constants.DEFAULT_GENRE
+            closest_genre = ''
+            score = 0
+        else:
+            closest_genre, score = process.extractOne(found_genre,
+                                                      constants.ALL_GENRES_LIST)
+
+            if score > constants.GENRE_SIMILARITY_THRESHOLD:
+                parent_genre = constants.GENRES_TREE[closest_genre]
+                decided_genre = parent_genre
+            else:
+                decided_genre = constants.DEFAULT_GENRE
 
     return decided_genre, closest_genre, score
 
