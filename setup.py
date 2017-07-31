f818863b29e76d7a7385847e5e5770dd360328ef
ormatt <ormatt@outlook.com>
2017-07-31 20:02:13 +0300

    Add functions for getting file path and reading files content

--- setup.py
+++ setup.py
@@ -7,21 +7,30 @@ __version__ = '0.1.0'
 
 SELF_DIR = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(SELF_DIR, 'requirements.txt')) as reader:
-    required = reader.read().splitlines()
+
+def get_full_path(path):
+    return os.path.join(SELF_DIR, path)
+
+
+def read_file(rel_path):
+    with open(get_full_path(rel_path)) as reader:
+        return reader.read()
+
+required = read_file('requirements.txt').splitlines()
 
 setup(name='Guess-Lyrics-By-Genre',
       version=__version__,
       install_requires=required,
       description="An attempt to guess a song's genre by its lyrics,"
                   "using ML models",
-      long_description=open(os.path.join(SELF_DIR, 'README.md')).read(),
+      long_description=read_file('README.md'),
       author='ormatt, Matt Murch',
       author_email='ormatt@outlook.com, mattmurch@gmail.com',
       url='https://github.com/ormatt/Guess-Lyrics-By-Genre',
       download_url='https://api.github.com/repos/ormatt/Guess-Lyrics-By-Genre/tarball/' + __version__,
-      scripts=['main.py'],
+      scripts=[get_full_path('main.py')],
       license='GPL - 3.0',
+      include_package_data=True,
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
@@ -29,11 +38,9 @@ setup(name='Guess-Lyrics-By-Genre',
           'Operating System :: OS Independent',
           'Programming Language :: Python :: 3',],
       keywords='lyrics genre guess* ML machine learning model',
-      packages=['data',
-                'data_cleaners',
-                'feat_exts',
-                'resources',
-                'transformers',
-                'utils',
+      packages=[get_full_path('data_cleaners'),
+                get_full_path('feat_exts'),
+                get_full_path('transformers'),
+                get_full_path('utils'),
                 ],
       )
