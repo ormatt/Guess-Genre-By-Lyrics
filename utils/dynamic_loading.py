17ed2e1dfa8676dacae69958547ff00261a500f6
ormatt <ormatt@outlook.com>
2017-08-02 00:55:34 +0300

    Add function for importing from modules, and an underscore prefix to internal functions

--- utils/dynamic_loading.py
+++ utils/dynamic_loading.py
@@ -1,48 +1,60 @@
 from fnmatch import fnmatch
 import os
 import os.path as path
-from constants import MODULE_PATTERN
+from constants import MODULE_PATTERN, SCRIPT_DIR
 from functools import reduce
 import imp
 
 
-def get_dir_content(s, _):
+def _get_dir_content(s, _):
     return s, map(lambda fn: path.join(s['modules_dir'], fn),
                   os.listdir(s['modules_dir']))
 
 
-def filter_only_files(s, dir_content):
+def _filter_only_files(s, dir_content):
     return s, filter(lambda fn: path.isfile(path.join(s['modules_dir'], fn)),
                      dir_content)
 
 
-def filter_by_pattern(s, dir_files):
+def _filter_by_pattern(s, dir_files):
     return s, filter(lambda fn:
                      fnmatch(path.basename(fn), MODULE_PATTERN),
                      dir_files)
 
 
-def import_modules(s, dir_filtered_files):
+def _import_modules(s, dir_filtered_files):
     return s, map(lambda fn:
                   imp.load_source(path.splitext(path.basename(fn))[0], fn),
                   dir_filtered_files)
 
 
-def get_objects(s, imported_modules):
+def _get_objects(s, imported_modules):
     return s, map(lambda mod:
                   getattr(mod, s['class_name']),
                   imported_modules)
 
 
-def objects_from_modules(modules_dir, class_name, args=None):
+def objects_from_modules(modules_dir, object_name, args=None):
     s = {'modules_dir': modules_dir,
-         'class_name': class_name,
+         'class_name': object_name,
          'args': args if args is not None else list()}
 
     return reduce(lambda func_a, func_b: func_b(*func_a),
-                  [get_dir_content,
-                   filter_only_files,
-                   filter_by_pattern,
-                   import_modules,
-                   get_objects],
+                  [_get_dir_content,
+                   _filter_only_files,
+                   _filter_by_pattern,
+                   _import_modules,
+                   _get_objects],
+                  (s, None))[1]
+
+
+def imports_from_modules(modules_dir, args=None):
+    s = {'modules_dir': modules_dir,
+         'args': args if args is not None else list()}
+
+    return reduce(lambda func_a, func_b: func_b(*func_a),
+                  [_get_dir_content,
+                   _filter_only_files,
+                   _filter_by_pattern,
+                   _import_modules],
                   (s, None))[1]
