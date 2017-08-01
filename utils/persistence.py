e84b2ab860a26de38820c04e496c596ae81dac8e
ormatt <ormatt@outlook.com>
2017-08-02 00:58:02 +0300

    Use sklearn's joblib module instead of pickle as it's more efficient with serializing numpy arrays

--- utils/persistence.py
+++ utils/persistence.py
@@ -1,21 +1,18 @@
 import os
+from sklearn.externals import joblib
+
 from constants import DUMPS_DIR
 from utils.logger import logger
-
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
 
 
 def dump(filename, obj):
     with open(os.path.join(DUMPS_DIR, filename), 'wb') as writer:
-        pickle.dump(obj, writer)
+        joblib.dump(obj, writer)
         logger.debug('Dumped obj with filename %s to disk' % filename)
 
 
 def load(filename):
     with open(os.path.join(DUMPS_DIR, filename), 'rb') as reader:
-        obj = pickle.load(reader)
+        obj = joblib.load(reader)
         logger.debug('Loaded obj with filename %s from disk' % filename)
     return obj
